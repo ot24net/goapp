b71185067983603b7d8c39b89d6d93e4f4116a6d
sy.zhou <sy.zhou@ot24.net>
2017-11-14 16:32:40 +0800

    fix env layout

--- init.bash
+++ init.bash
@@ -5,31 +5,21 @@ read project_name
 
 echo '#!/bin/bash
 
-# 需要导出的程序环境变量
+# 项目需要导出的程序环境变量
+# -------------------------------------------------
 export PJ_ROOT=`pwd`
 export PJ_NAME="'$project_name'"
+export GOLIBS="$(dirname "$PJ_ROOT")/golibs" # 可作为公共库时使用
 
+# 用于目录的快速跳转变量
+export github=$GOLIBS/src/github.com
+
+# 设定sup [command] all 的遍历目录
+export build_all="$PJ_ROOT/src/app $PJ_ROOT/src/web"
 
 # 以下配置可使用默认配置即可 
 # --------------------START-------------------
-# 设定GO编译环境
-export GOLIBS="$(dirname "$PJ_ROOT")/golibs" # 可作为公共库时使用
-export GOPATH=$GOLIBS:$PJ_ROOT
-export GOBIN=$PJ_ROOT/bin
-
-# 更改路径可更改编译器的版本号, 如果未指定，使用系统默认的配置
-go_root="/usr/local/go"
-if [ -d "$go_root" ]; then
-    export GOROOT="$go_root"
-fi
-
-bin_path=$GOBIN:$GOROOT/bin
-if [[ ! $PATH =~ $bin_path ]]; then
-	export PATH=$bin_path:$PATH
-fi
-# -------------------------------------------------
-
-# 设定SUP发布环境
+# 设定sup部署使用的环境
 # 以下是部署时的supervisor默认配置数据，若未配置时，会使用以下默认数据
 # 开发IDE可不配置以下环境变量
 # 配置supervisor的配置文件目录
@@ -40,37 +30,44 @@ export SUP_LOG_SIZE="10MB"
 export SUP_LOG_BAK="10"
 # 配置supervisor配置中的environment环境变量
 export SUP_APP_ENV="PJ_ROOT=\\\"$PJ_ROOT\\\",GIN_MODE=\\\"release\\\",LD_LIBRARY_PATH=\\\"$LD_LIBRARY_PATH\\\""
-# 设定sup [command] all 的目录
-export SUP_BUILD_PATH="$PJ_ROOT/src/app $PJ_ROOT/src/web"
+# 设定sup [command] all 的遍历目录
+export SUP_BUILD_PATH=$build_all
 # -------------------------------------------------
-
-# 设定publish指令打包时需要包含的文件夹
-# 默认会打包：$PJ_ROOT/bin $PJ_ROOT/src/app/app等二进制程序
-export PUB_ROOT_RES="" # 根目录下的文件夹列表，如"etc res"等
+# 设定publish指令打包时需要包含的文件夹环境变量
+# 默认会打包以下目录：$PJ_ROOT/bin/* $PJ_ROOT/src/app/app等二进制程序
+export PUB_ROOT_RES="" # 根目录下需要打包的文件夹列表，如"etc res"等
 export PUB_APP_RES="public" # app下的文件夹列表，如"etc public"等
 
-
-# 构建项目目录
-mkdir -p $PJ_ROOT/src
-mkdir -p $PJ_ROOT/log
-
-# 下载自定义goget管理工具
-if [ ! -f $PJ_ROOT/bin/sup ]; then
-	sup_path="gopkg.in/ot24net/sup.v3"
-	go get -u -v $sup_path
-    go install $sup_path/goget
-    # TODO: code sup by golang
-	cp -rf $GOLIBS/src/$sup_path/sup $PJ_ROOT/bin
+# 更改路径可更改编译器的版本号, 如果未指定，使用系统默认的配置
+export GOPATH=$GOLIBS:$PJ_ROOT
+export GOBIN=$PJ_ROOT/bin
+go_root="/usr/local/go"
+if [ -d "$go_root" ]; then
+    export GOROOT="$go_root"
 fi
 
-# 设定git库地址转换, 以便解决私有库中https证书不可信的问题
-# git config --global url."git@git.ot24.net:".insteadOf "https://git.ot24.net"
-# 用于快速跳转文件变量
-export github=$GOLIBS/src/github.com
+bin_path=$GOBIN:$GOROOT/bin
+if [[ ! $PATH =~ $bin_path ]]; then
+	export PATH=$bin_path:$PATH
+fi
+
+# 构建项目目录
+mkdir -p $PJ_ROOT/src || exit 1
+mkdir -p $PJ_ROOT/log || exit 1
+
+# 下载自定义goget管理工具
+if [ ! -f $GOBIN/sup ]; then
+	sup_pkg="gopkg.in/ot24net/sup.v3"
+	go get -u -v $sup_pkg || exit 1
+    go install $sup_pkg/goget || exit 1
+    # TODO: code sup by golang
+	cp -rf $GOLIBS/src/$sup_pkg/sup $GOBIN/ || exit 1
+fi
+# --------------------END--------------------
 
 echo "Env have changed to \"$PJ_NAME\""
 echo "Using \"sup help\" to manage project"
-# --------------------END--------------------
+# -------------------------------------------------
 '>env.bash
 
 echo '#' "Init Done"
